d5ce2cad7f20c857aebb200d49f45ff26b1bc17e
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-08-19 17:25:11 -0400

    Add template for carousel

--- leone-sisters/src/app/app.module.ts
+++ leone-sisters/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
